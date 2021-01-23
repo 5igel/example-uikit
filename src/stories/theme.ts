7fb69b7a3c1392cbddc1a8caf28108d25cea0981
Sergii Kocherev <st.ixidor@gmail.com>
2021-01-23 21:34:17 +0200

    added theme values

--- src/stories/theme.ts
+++ src/stories/theme.ts
@@ -1,8 +1,13 @@
 export default {
   colors: {
-    black: '#000e1a',
-    white: '#fff',
-    blue: '#007ce0',
-    navy: '#004175',
+    primary: '#0063D9',
+    secondary: '#E7E7E7',
+    tertiary: '#AAAAAA',
+    danger: '#CC2E2E',
+    success: '#3FBA69',
+    background: '#FFFFFF',
+    foreground: '#333333'
   },
+  fontSizes: [14, 16, 20, 24, 28, 32, 36],
+  space: [0, 4, 8, 16, 32, 64]
 }
